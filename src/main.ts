c0bf82c1958a49e92ae52617379e62a2e3ba3e56
Button501 <51501719+SodeButton@users.noreply.github.com>
2024-04-17 18:46:34 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { GatewayIntentBits, Partials, Message } from 'discord.js';
 import Client from './client';
 import dotenv from 'dotenv';
 import * as fs from "fs";
+import express from 'express';
+
+const app = express();
+app.listen(3000, () => {
+	console.log("Server is running!");
+});
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
 
 //.envファイルを読み込む
 dotenv.config();
